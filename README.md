b523c996197a3debd1bfc1758c74a528f0f3da2e
Roman-Port <rvporterfield@gmail.com>
2020-04-25 14:11:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Acura Hands-Free-Mode Cracker
+
+Software designed for an Arduino to crack the four digit pin code on early 2000s Acura hands free mode systems.
+
+## Why?
+tl;dr; neither the previous owner or I know the pin code to the hands free system in this Acura. I'd like to get it working, but it's locked behind this code.
---
+++ SoundConverter/SoundConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SoundConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "const unsigned char voicedata_data[][$REPLACE_MAX] PROGMEM = {";
+            string output2 = "const unsigned int voicedata_length[] = {";
+            int max = 0;
+            for(int i = 0; i<10; i++)
+            {
+                byte[] data = File.ReadAllBytes(@"C:\Users\Roman\Desktop\Voice Test\" + i + ".raw");
+                output += "\n{";
+                foreach (byte b in data)
+                    output += b.ToString() + ",";
+                output = output.TrimEnd(',');
+                output += "},";
+                output2 += data.Length.ToString() + ",";
+                if (data.Length > max)
+                    max = data.Length;
+            }
+            output2 = output2.TrimEnd(',') + "};";
+            output += "\n};";
+            output = output.Replace("$REPLACE_MAX", max.ToString());
+            File.WriteAllText("output.txt", output2 + "\n\n" + output);
+        }
+    }
+}
